15afb47d30cf16ec34ce1409c218dbe45c66abd5
George Dzuricsko & Jim Kyle <pair+geoclimber4.jwkyle@devbootcamp.com>
2017-08-11 15:39:18 -0500

    add decks/index.erb, update deck model for typo and decks_controller

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,1 +1,5 @@
 
+get '/decks' do
+  @decks = Deck.all
+  erb :'/decks/index'
+end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,8 +1,8 @@
 class Deck < ActiveRecord::Base
   validates :name, presence: true
 
-  has_many: cards
-  belongs_to: round
+  has_many :cards
+  belongs_to :round
 
   # Remember to create a migration!
 end
