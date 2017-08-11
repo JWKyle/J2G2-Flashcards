6e2ea56e6f4ce0261cc8abe575c414a0b2495849
Gwynne Noth & jalletto <pair+genoth.jalletto@devbootcamp.com>
2017-08-11 17:00:40 -0500

    user can log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ get '/sessions/new' do
 end
 
 post '/sessions' do
-  user = User.authenticate(params[:username], params[:password])
-  if user
-    session[:user_id] = user.id
+  @user = User.authenticate(params[:user_name], params[:password_text])
+  if @user
+    session[:user_id] = @user.id
     redirect '/'
   else
     erb :'/sessions/new'
