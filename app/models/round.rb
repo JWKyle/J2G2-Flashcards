e9b11952fd42531bb379df4c4ffb2b8dce51f1fe
George Dzuricsko & Jim Kyle <pair+geoclimber4.jwkyle@devbootcamp.com>
2017-08-12 14:11:35 -0500

    add associations. add next_card, first_try_correct methods

--- app/models/round.rb
+++ app/models/round.rb
@@ -4,7 +4,22 @@ class Round < ActiveRecord::Base
 
   belongs_to :deck
   belongs_to :user
+  has_many :guesses
+  has_many :cards, through: :deck
 
   # next_card method should live here?
+
+  def next_card
+    self.deck.cards.select do |card|
+      card.guesses.pluck(:correct) == 0
+    end
+  end
+
+  def first_try_correct
+    self.deck.cards.select do |card|
+      (card.guesses.pluck(:tries)== 1) && (card.guesses.pluck(:correct) == 1)
+
+    end
+  end
 
 end
